8b89e3c244a1f68ad0326489260699fec546bf33
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-10 21:47:42 +0530

    Implement web search and result formatting functions
    Added functions to search the web and format results.

--- search.py
+++ search.py
@@ -1,1 +1,29 @@
+from ddgs import DDGS
 
+def search_web(query, max_results=3):
+    """
+    Search the web and return a list of results.
+    """
+
+    with DDGS() as ddgs:
+        results = list(ddgs.text(query, max_results=max_results))
+
+    return results
+
+
+def format_results(results):
+    """
+    Convert search results into text that the AI can read.
+    """
+
+    text = ""
+
+    for result in results:
+        text += f"""
+Title: {result['title']}
+Body: {result['body']}
+URL: {result['href']}
+
+"""
+
+    return text
