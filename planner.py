8d2dc5fe19312f59a3c4e16dfd25b119daf3853c
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-12 02:51:31 +0530

    Update planner.py

--- planner.py
+++ planner.py
@@ -1,1 +1,11 @@
+# ==========================
+# DEEK AI PLANNER
+# ==========================
 
+class Planner:
+
+    def create_plan(self, request):
+
+        return [
+            request.intent
+        ]
