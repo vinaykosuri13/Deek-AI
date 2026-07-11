72c0f15fe432949758561b5d3a84cd37a98e8c14
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-12 02:15:32 +0530

    Update memory_tool.py

--- memory_tool.py
+++ memory_tool.py
@@ -1,1 +1,12 @@
+# ==========================
+# DEEK AI MEMORY TOOL
+# ==========================
 
+from memory import add_message
+
+
+def memory_tool(role, message):
+
+    add_message(role, message)
+
+    return "Memory Updated"
