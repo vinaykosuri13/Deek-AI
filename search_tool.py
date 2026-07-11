3f4a5b2959cc18af2da7b98ec789c5dff65e899c
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-12 02:59:49 +0530

    Refactor search_tool to use request object

--- search_tool.py
+++ search_tool.py
@@ -7,11 +7,13 @@ from ai import ask
 from response import Response
 
 
-def search_tool(question):
+def search_tool(request):
 
-    results = search_web(question)
+    results = search_web(request.question)
 
     context = format_results(results)
+
+    request.context = context
 
     prompt = f"""
 Use these search results to answer the user's question.
@@ -21,7 +23,7 @@ Search Results:
 {context}
 
 Question:
-{question}
+{request.question}
 """
 
     answer = ask(prompt)
