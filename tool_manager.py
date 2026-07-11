7269cbaf89aba72c1c496b0d3fdc5eeb24491999
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-12 02:09:53 +0530

    Update tool_manager.py

--- tool_manager.py
+++ tool_manager.py
@@ -2,8 +2,19 @@
 # DEEK AI TOOL MANAGER
 # ==========================
 
+from search_tool import search_tool
+from chat_tool import chat_tool
+
+
 def execute_tool(intent, question):
 
     print(f"Selected Tool: {intent}")
 
-    return f"{intent}: {question}"
+    if intent == "SEARCH":
+        return search_tool(question)
+
+    elif intent == "CHAT":
+        return chat_tool(question)
+
+    else:
+        return "Tool not implemented."
