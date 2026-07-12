eba7acb9b479b7a4c9cf69b81d1c94830ffaac6f
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-13 00:31:10 +0530

    Add datetime_rule to planner rules

--- planner.py
+++ planner.py
@@ -5,10 +5,12 @@
 from planner_rules.memory_rule import match as memory_rule
 from planner_rules.calculator_rule import match as calculator_rule
 from planner_rules.search_rule import match as search_rule
+from planner_rules.datetime_rule import match as datetime_rule
 
 RULES = [
     memory_rule,
     calculator_rule,
+    datetime_rule,
     search_rule,
 ]
 
