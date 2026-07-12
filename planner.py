fdc8709cf3ea726107d946e28bc4a67a1a53ece3
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-12 23:56:01 +0530

    Refactor Planner to use rule-based approach

--- planner.py
+++ planner.py
@@ -2,43 +2,27 @@
 # DEEK AI PLANNER
 # ==========================
 
+from planner_rules.memory_rule import match as memory_rule
 from planner_rules.calculator_rule import match as calculator_rule
+
+RULES = [
+    memory_rule,
+    calculator_rule,
+]
 
 
 class Planner:
 
     def create_plan(self, request):
 
+        for rule in RULES:
+
+            plan = rule(request)
+
+            if plan:
+                return plan
+
         question = request.question.lower()
-
-        # Memory retrieval
-        if (
-            "what's my" in question or
-            "what is my" in question or
-            "do you remember" in question
-        ):
-            return [
-                {"tool": "MEMORY_SEARCH"},
-                {"tool": "CHAT"}
-            ]
-
-        # Memory save
-        if (
-            "my name is" in question or
-            "i am" in question or
-            "my favorite" in question or
-            "i like" in question
-        ):
-            return [
-                {"tool": "CHAT"},
-                {"tool": "MEMORY"}
-            ]
-
-        # Calculator Rule
-        plan = calculator_rule(request)
-
-        if plan:
-            return plan
 
         # Search then summarize
         if "summarize" in question:
