b26583d2e39da2bb1b76a3fe6f68e35c0a36bb3b
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-13 01:00:21 +0530

    Refactor Response class constructor to include optional fields

--- response.py
+++ response.py
@@ -4,8 +4,22 @@
 
 class Response:
 
-    def __init__(self, success, message, source):
+    def __init__(
+        self,
+        success,
+        message,
+        source,
+        data=None,
+        results=None,
+        metadata=None
+    ):
 
         self.success = success
         self.message = message
         self.source = source
+
+        self.data = data if data is not None else {}
+
+        self.results = results if results is not None else []
+
+        self.metadata = metadata if metadata is not None else {}
