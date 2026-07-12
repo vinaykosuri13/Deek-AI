26aa02d33b2895c107f6930b289f9b1129de0195
vinaykosuri13 <vinaykosuri90@gmail.com>
2026-07-13 00:49:12 +0530

    Create weather_service.py

---
+++ weather_service.py
@@ -0,0 +1,44 @@
+# ==========================
+# DEEK AI WEATHER SERVICE
+# ==========================
+
+import requests
+
+
+def get_coordinates(city):
+
+    url = "https://geocoding-api.open-meteo.com/v1/search"
+
+    response = requests.get(url, params={
+        "name": city,
+        "count": 1
+    })
+
+    data = response.json()
+
+    if "results" not in data:
+        return None
+
+    result = data["results"][0]
+
+    return result["latitude"], result["longitude"]
+
+
+def get_weather(city):
+
+    location = get_coordinates(city)
+
+    if location is None:
+        return None
+
+    latitude, longitude = location
+
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    response = requests.get(url, params={
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": "temperature_2m,weather_code"
+    })
+
+    return response.json()
